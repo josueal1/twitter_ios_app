fa756e353bbf4a80a7af00335a49c340157a4a1d
Josue Lopez <josueal1@uci.edu>
2019-02-10 17:20:45 -0800

    embedded favorited action with API caller

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -13,7 +13,42 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var tweetContentLabel: UILabel!
     @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var favButton: UIButton!
     
+    var favorited:Bool = false
+    var tweetID: Int = -1
+    
+    @IBAction func reTweet(_ sender: Any) {
+        // change icon
+        // send api to retweet
+    }
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        // CONDENSE THIS FAVORITE CODE
+        let toBeFavorited = !favorited
+        if (toBeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetID: tweetID, success: {self.setFavorite(true)}, failure: {(error) in print("Favorited did not succeed \(error)")})
+        }
+        else{
+            TwitterAPICaller.client?.unfavoriteTweet(tweetID: tweetID, success: {self.setFavorite(false)}, failure: {(error) in print("Unfavorited did not succeed \(error)")})
+        }
+        // mark down as a favorite tweet for my account
+    }
+    
+    // this boolean status holds our data about a tweet
+
+    
+    func setFavorite(_ isFavorite:Bool){
+        favorited = isFavorite
+        if (favorited){
+            favButton.setImage(UIImage(named:"favor-icon-red"), for: UIControl.State.normal)
+        }
+        else{
+            favButton.setImage(UIImage(named:"favor-icon"), for: UIControl.State.normal)
+        }
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
